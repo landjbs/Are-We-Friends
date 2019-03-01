e3042e00ad5b63823122b655b4cbafb1ac23036f
landjbs <landonsmith@college.harvard.edu>
2019-03-01 17:52:23 -0500

    class altered

--- app.py
+++ app.py
@@ -39,24 +39,9 @@ def home():
     # On form entry and all conditions met
     if request.method == 'POST' and form.validate():
         # Extract information
-        seed = request.form['seed']
-        diversity = float(request.form['diversity'])
-        words = int(request.form['words'])
+        message = request.form['message']
         # Generate a random sequence
-        if seed == 'random':
-            return render_template('random.html',
-                                   input=generate_random_start(model=model,
-                                                               graph=graph,
-                                                               new_words=words,
-                                                               diversity=diversity))
-        # Generate starting from a seed sequence
-        else:
-            return render_template('seeded.html',
-                                   input=generate_from_seed(model=model,
-                                                            graph=graph,
-                                                            seed=seed,
-                                                            new_words=words,
-                                                            diversity=diversity))
+
     # Send template information to index.html
     return render_template('index.html', form=form)
 
@@ -95,17 +80,7 @@ DecimalField, IntegerField)
 class ReusableForm(Form):
     """User entry form for entering specifics for generation"""
     # Starting seed
-    seed = TextField("Enter a seed string or 'random':", validators=[
+    message = TextField("Enter a sample message:", validators=[
                      validators.InputRequired()])
-    # Diversity of predictions
-    diversity = DecimalField('Enter diversity:', default=0.8,
-                             validators=[validators.InputRequired(),
-                                         validators.NumberRange(min=0.5, max=5.0,
-                                         message='Diversity must be between 0.5 and 5.')])
-    # Number of words
-    words = IntegerField('Enter number of words to generate:',
-                         default=50, validators=[validators.InputRequired(),
-                                                 validators.NumberRange(min=10, max=100,
-                                                 message='Number of words must be between 10 and 100')])
     # Submit button
     submit = SubmitField("Enter")

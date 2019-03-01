f21679e803ff57aae868efb9af6e2b0594fab0ac
landjbs <landonsmith@college.harvard.edu>
2019-03-01 17:41:49 -0500

    updates to app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
+import pickle
 
 ### WEB APP ###
 # Configure application
@@ -32,7 +33,10 @@ Session(app)
 @app.route("/")
 def index():
 	return render_template('index.html')
-    
+
+loaded_model = pickle.load(open("facebookModel.sav", "rb"))
+# result = loaded_model.predict(np.expand_dims(userData, axis=1).T)
+
 #
 # @app.route("/create", methods=["GET", "POST"])
 # def create(phraseList=phraseList, scoresList=scoresList):
@@ -58,3 +62,24 @@ def index():
 # 		print("post")
 # 	else:
 # 		return render_template('register.html')
+
+from wtforms import (Form, TextField, validators, SubmitField,
+DecimalField, IntegerField)
+
+class ReusableForm(Form):
+    """User entry form for entering specifics for generation"""
+    # Starting seed
+    seed = TextField("Enter a seed string or 'random':", validators=[
+                     validators.InputRequired()])
+    # Diversity of predictions
+    diversity = DecimalField('Enter diversity:', default=0.8,
+                             validators=[validators.InputRequired(),
+                                         validators.NumberRange(min=0.5, max=5.0,
+                                         message='Diversity must be between 0.5 and 5.')])
+    # Number of words
+    words = IntegerField('Enter number of words to generate:',
+                         default=50, validators=[validators.InputRequired(),
+                                                 validators.NumberRange(min=10, max=100,
+                                                 message='Number of words must be between 10 and 100')])
+    # Submit button
+    submit = SubmitField("Enter")
